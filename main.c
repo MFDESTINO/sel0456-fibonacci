4e8d02a2f54e6532e3260011b0d19e60b793f67b
couto0 <daniel.couto64@gmail.com>
2022-09-15 13:07:51 -0300

    for version files

--- main.c
+++ main.c
@@ -14,12 +14,6 @@ void fib_for(int n){
 }
 
 
-int fib_rec(int n){
-    if(n==0)return 0;
-    if(n==1)return 1;
-    return fib_rec(n-1) + fib_rec(n-2);
-}
-
 int main() {
     fib_for(10);
     return 0;
